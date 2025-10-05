c881fb83ad6858febe7911d808057fde63bf4503
IsaacFeld <isaacbfeldman@gmail.com>
2025-10-05 15:20:12 +0200

    Setup Express Server - Finished Copying over Figma Design into Website

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+const port = 3000
+
+app.get('/alert', (req, res) => {
+    const email = req.query.email ? req.query.email : null;
+    console.log(email)
+    res.send('<h3> Alert Creation is not setup yet, please come back later. </h3>')
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ', port)
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+A node app responsible for visualizing and storing water temperature tracking data. 
+
